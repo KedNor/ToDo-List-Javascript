3226779b53c227a9a3f8ab52770af1b4f27f5519
Keddar Noreddine <noreddinekeddar@gmail.com>
2021-11-17 23:41:11 +0100

    Ajouter Todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-import "./style.css";
+// import "./style.css";
+
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
+
+console.log(form, input);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entre = input.value;
+  input.value = "";
+  addTodo(entre);
+  displayTodo();
+});
+
+const todo = [
+  {
+    text: "Je suis une todo",
+    done: false,
+  },
+
+  {
+    text: "faire du JS",
+    done: true,
+  },
+];
+
+const displayTodo = () => {
+  const todosNode = todo.map((todo, index) => {
+    return createTodoElement(todo, index);
+  });
+  ul.innerHTML = "";
+  ul.append(...todosNode);
+};
+
+const createTodoElement = (todo, index) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}"> </span>
+    <p>${todo.text}</p>
+    <button>Supprimer</button>`;
+  return li;
+};
+
+const addTodo = (text) => {
+  todo.push({
+    text,
+    done: false,
+  });
+};
+
+displayTodo();
